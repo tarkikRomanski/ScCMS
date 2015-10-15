780781f87e55719535a8f45fe81fda0376c7e8fd
Tarkik Romanski <tarkik@ukr.net>
2015-10-16 01:48:28 +0300

    add full namespace `Connector` and class connector

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "connector.php";
+
+use Connector\connector as conn;
+
+$connect = new conn("Main");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Main page</title>
+    </head>
+    <body>
+        <h1>Just large text</h1>
+        <p>Just text</p>
+    </body>
+</html>
